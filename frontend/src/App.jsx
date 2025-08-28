8c41193c70e8879562bb9f7150dbded6924b4fa3
amantiwary12 <amantiwary2505@gmail.com>
2025-08-28 13:00:44 +0530

    hello

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,28 +22,3 @@ return (
 export default App;
 
 
-
-
-//// using redxux to fetch data from backend
-// src/App.jsx
-// import { useEffect } from "react";
-// import { useDispatch } from "react-redux";
-// import { fetchUsers } from "./features/users/userSlice";
-// import Page1 from "./pages/Page1";
-// import Page2 from "./pages/Page2";
-
-// export default function App() {
-//   const dispatch = useDispatch();
-
-//   useEffect(() => {
-//     dispatch(fetchUsers()); // fetch user data once at start
-//   }, [dispatch]);
-
-//   return (
-//     <div>
-//       <h1>React Redux User Fetch Example</h1>
-//       <Page1 />
-//       <Page2 />
-//     </div>
-//   );
-// }
